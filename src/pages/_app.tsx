d6d2b2629abb7b8c67a08cb97c1cacd504e430c3
personal <reddyarun59@yahoo.com>
2024-01-07 04:02:25 +0530

    added header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { EmployeeTreeProvider } from "@/components/EmployeeTreeProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <EmployeeTreeProvider>
-      <Component {...pageProps} />
-    </EmployeeTreeProvider>
+    <div>
+      <Head>
+        <title>Org Chart</title>
+        <meta name="description" content="Org Chart visualisation app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <EmployeeTreeProvider>
+        <Component {...pageProps} />
+      </EmployeeTreeProvider>
+    </div>
   );
 }
